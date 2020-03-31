e101c4ac1bec74a31b837682c071a4b538071f62
Kavachi Ukegbu <michelle.ukegbu@gmail.com>
2020-03-31 23:27:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -17,3 +17,7 @@ return array
 function accessElementInArray(array, index) {
 return array [index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+array.shift(element)
+return array
+}
